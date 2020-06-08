d9ed370f531f2a66138523367d502c06dcd90321
DoyKim-20 <diana7533@naver.com>
2020-06-08 21:25:01 +0900

    message

--- page/views.py
+++ page/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
+
 def home(request):
    return render(request,'home.html')
 
 def about(request):
+    
     return render(request,'about.html')
 
 def result(request):
-    return render(request,'result.html')
+
+    number_list=list()
+    
+    for i in range(6):
+        number=request.GET['number'+str(i+1)]
+        number_list.append(int(number))
+
+    random_list=list()
+
+    for j in range(7):
+        number=random.randrange(1,46)
+        while number in random_list:
+            number=random.randrange(1,46)
+        random_list.append(number)
+    
+    count=0
+
+    for i in range(6):
+        for j in range(7):
+            if(number_list[i]==random_list[j]):
+                count+=1
+
+
+    return render(request,'result.html',{'number_list:number_list':number_list,'random_list':random_list,'count':count})
